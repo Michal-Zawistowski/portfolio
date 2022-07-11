b7c55f2efb4ece132ddb759abe08b9a75840e91c
Michal-Zawistowski <zawistowskimichal1@gmail.com>
2022-07-11 22:12:30 +0200

    tester

--- script.js
+++ script.js
@@ -32,14 +32,14 @@ $(document).ready(function(){
 
     // typing animation script
     var typed = new Typed(".typing",{
-        strings: ["Jr Test Automation Engineer ", "Freelancer"],
+        strings: ["Jr Test Automation Engineer "],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2",{
-        strings: ["Jr Test Automation Engineer", "Freelancer"],
+        strings: ["Jr Test Automation Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
